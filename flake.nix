81bd92d97245005807ab79a5568f4a1ba0fd5b07
Andrew Bruce <me@andrewbruce.net>
2024-09-29 13:08:06 +0100

    Basic shell with Elixir and direnv

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs }:
+    let
+      pkgs =
+        nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux.default = with pkgs; mkShell {
+        packages = [
+          elixir_1_17
+        ];
+      };
+    };
 }
