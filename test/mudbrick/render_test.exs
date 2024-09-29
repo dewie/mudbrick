6bef8760e8e6d56710a67276566cd2ddd3076ab9
Andrew Bruce <me@andrewbruce.net>
2024-09-29 17:34:00 +0100

    It turns out we don't need CRLF, LF will do.

--- test/mudbrick/render_test.exs
+++ test/mudbrick/render_test.exs
@@ -2,8 +2,7 @@ defmodule Mudbrick.RenderTest do
   use ExUnit.Case, async: true
 
   @lf 10
-  @cr 13
-  @eol <<@cr, @lf>>
+  @eol <<@lf>>
 
   test "version is always 2.0" do
     assert <<"%PDF-2.0", @eol, _rest::binary>> = Mudbrick.new() |> Mudbrick.render()
