ecfac924aa6684c1e9597b699d1c7625f7e3cea8
Andrew Bruce <me@andrewbruce.net>
2024-10-10 15:40:37 +0100

    Set up shell for Mac

--- flake.nix
+++ flake.nix
@@ -5,21 +5,29 @@
 
   outputs = { self, nixpkgs }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
     in
     {
-      devShells.x86_64-linux.default = with pkgs; mkShell {
-        packages = [
-          elixir_1_17
-          (elixir_ls.override { elixir = elixir_1_17; })
-          inotify-tools
-          qpdf
-        ];
+      devShells = forAllSystems (pkgs: {
+        default = with pkgs; mkShell {
+          packages = [
+            elixir_1_17
+            (elixir_ls.override { elixir = elixir_1_17; })
+            qpdf
+          ]
+          ++ lib.lists.optional stdenv.isLinux inotify-tools
+          ++ lib.lists.optional stdenv.isDarwin darwin.apple_sdk.frameworks.CoreServices;
 
-        shellHook = ''
-          export ERL_AFLAGS="-kernel shell_history enabled shell_history_file_bytes 1024000"
-          export FONT_LIBRE_BODONI_REGULAR="${libre-bodoni}/share/fonts/opentype/LibreBodoni-Regular.otf"
-        '';
-      };
+          shellHook = ''
+            export ERL_AFLAGS="-kernel shell_history enabled shell_history_file_bytes 1024000"
+            export FONT_LIBRE_BODONI_REGULAR="${libre-bodoni}/share/fonts/opentype/LibreBodoni-Regular.otf"
+          '';
+        };
+      });
     };
 }
