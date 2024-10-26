6c2dbb22b328f5871cb72a5eb0a7440bd78f21a0
Andrew Bruce <me@andrewbruce.net>
2024-10-26 09:34:26 +0100

    Right-aligned text block

--- lib/mudbrick/text_block.ex
+++ lib/mudbrick/text_block.ex
@@ -1,5 +1,6 @@
 defmodule Mudbrick.TextBlock do
-  defstruct font: nil,
+  defstruct align: :left,
+            font: nil,
             font_size: nil,
             operations: [],
             position: {0, 0}
@@ -9,31 +10,91 @@ defmodule Mudbrick.TextBlock do
   alias Mudbrick.ContentStream.Tf
   alias Mudbrick.ContentStream.{Tj, Apostrophe}
   alias Mudbrick.ContentStream.TL
+  alias Mudbrick.Font
 
   def new(opts \\ []) do
     struct!(__MODULE__, opts)
   end
 
-  def write(%__MODULE__{font: font, font_size: font_size, position: {x, y}} = tb, text) do
-    tb =
-      tb
-      |> add(%BT{})
-      |> add(%Td{tx: x, ty: y})
-      |> add(%Tf{font: font, size: font_size})
-      |> add(%TL{leading: font_size * 1.2})
+  def write(%__MODULE__{align: :right} = tb, text) do
+    tb = start(tb)
 
-    case String.split(text, "\n") do
+    split(text, fn
       [first | rest] ->
         tb
+        |> right_text(first, 1)
+        |> then(fn context ->
+          {_n, context} =
+            for text <- rest, reduce: {2, context} do
+              {line, acc} ->
+                {
+                  line + 1,
+                  acc
+                  |> start_block()
+                  |> right_text(text, line)
+                }
+            end
+
+          context
+        end)
+    end)
+  end
+
+  def write(%__MODULE__{position: {x, y}} = tb, text) do
+    tb = start(tb)
+
+    split(text, fn
+      [first | rest] ->
+        tb
+        |> add(%Td{tx: x, ty: y})
         |> tj(first)
         |> then(
           &for text <- rest, reduce: &1 do
-            acc ->
-              apos(acc, text)
+            acc -> apos(acc, text)
           end
         )
-    end
-    |> add(%ET{})
+    end)
+    |> end_block()
+  end
+
+  defp split(text, f) do
+    f.(String.split(text, "\n"))
+  end
+
+  defp right_text(tb, text, line) do
+    tb
+    |> right_offset(text, line)
+    |> tj(text)
+    |> end_block()
+  end
+
+  defp right_offset(tb, text, line) do
+    n = line - 1
+    {x, y} = tb.position
+
+    add(tb, %Td{
+      tx: x - Font.width(tb.font, tb.font_size, text),
+      ty: y - leading(tb) * n
+    })
+  end
+
+  defp start(%__MODULE__{font: font, font_size: font_size} = tb) do
+    tb
+    |> start_block()
+    |> add(%Tf{font: font, size: font_size})
+    |> add(%TL{leading: leading(tb)})
+  end
+
+  defp start_block(tb) do
+    add(tb, %BT{})
+  end
+
+  defp end_block(tb) do
+    add(tb, %ET{})
+  end
+
+  defp leading(tb) do
+    tb.font_size * 1.2
   end
 
   defp add(%__MODULE__{} = tb, op) do
--- test/mudbrick/text_block_test.exs
+++ test/mudbrick/text_block_test.exs
@@ -1,26 +1,20 @@
 defmodule Mudbrick.TextBlockTest do
-  alias Mudbrick.ContentStream.Apostrophe
   use ExUnit.Case, async: true
 
   import Mudbrick.TestHelper, only: [bodoni: 0]
 
-  alias Mudbrick.ContentStream.{BT, ET}
-  alias Mudbrick.ContentStream.Td
-  alias Mudbrick.ContentStream.Tf
-  alias Mudbrick.ContentStream.{Tj, Apostrophe}
-  alias Mudbrick.ContentStream.TL
   alias Mudbrick.Page
 
   test "left-aligned newlines become apostrophes" do
     assert [
-             %BT{},
-             %Td{tx: 400, ty: 500},
-             %Tf{size: 10},
-             %TL{leading: 12.0},
-             %Tj{text: "first line"},
-             %Apostrophe{text: "second line"},
-             %Apostrophe{text: ""},
-             %ET{}
+             "BT",
+             "/F1 10 Tf",
+             "12.0 TL",
+             "400 500 Td",
+             "<014C010F0116011D01B700ED00D900F400C0> Tj",
+             "<011600C000B500FC00F400BB01B700ED00D900F400C0> '",
+             "() '",
+             "ET"
            ] =
              output(fn font ->
                Mudbrick.TextBlock.new(
@@ -36,8 +30,80 @@ defmodule Mudbrick.TextBlockTest do
              |> operations()
   end
 
+  test "right-aligned newlines become Tjs with offsets" do
+    assert [
+             "BT",
+             "/F1 10 Tf",
+             "12.0 TL",
+             "384.82 500.0 Td",
+             "<00A500A500A5> Tj",
+             "ET",
+             "BT",
+             "379.42 488.0 Td",
+             "<013801380138> Tj",
+             "ET",
+             "BT",
+             "306.48 476.0 Td",
+             "<00880055008800550088005500880055008800550088> Tj",
+             "ET",
+             "BT",
+             "400 464.0 Td",
+             "() Tj",
+             "ET"
+           ] =
+             output(fn font ->
+               Mudbrick.TextBlock.new(
+                 font: font,
+                 font_size: 10,
+                 position: {400, 500},
+                 align: :right
+               )
+               |> Mudbrick.TextBlock.write("""
+               aaa
+               www
+               WOWOWOWOWOW
+               """)
+             end)
+             |> operations()
+  end
+    assert [
+             "BT",
+             "/F1 10 Tf",
+             "12.0 TL",
+             "384.82 500.0 Td",
+             "<00A500A500A5> Tj",
+             "ET",
+             "BT",
+             "379.42 488.0 Td",
+             "<013801380138> Tj",
+             "ET",
+             "BT",
+             "306.48 476.0 Td",
+             "<00880055008800550088005500880055008800550088> Tj",
+             "ET",
+             "BT",
+             "400 464.0 Td",
+             "() Tj",
+             "ET"
+           ] =
+             output(fn font ->
+               Mudbrick.TextBlock.new(
+                 font: font,
+                 font_size: 10,
+                 position: {400, 500},
+                 align: :right
+               )
+               |> Mudbrick.TextBlock.write("""
+               aaa
+               www
+               WOWOWOWOWOW
+               """)
+             end)
+             |> operations()
+  end
+
   defp operations(block) do
-    Enum.reverse(block.operations)
+    Enum.reverse(block.operations) |> Enum.map(&Mudbrick.TestHelper.show/1)
   end
 
   defp output(f) when is_function(f) do
