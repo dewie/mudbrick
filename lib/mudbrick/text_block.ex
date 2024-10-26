5114abfe47f12fcbdd5ffe861f11592d7c9ae49c
Andrew Bruce <me@andrewbruce.net>
2024-10-26 13:27:12 +0100

    AST for text block

--- lib/mudbrick/text_block.ex
+++ lib/mudbrick/text_block.ex
@@ -2,9 +2,134 @@ defmodule Mudbrick.TextBlock do
   defstruct align: :left,
             font: nil,
             font_size: nil,
-            operations: [],
+            lines: [],
             position: {0, 0}
 
+  defmodule Line do
+    defstruct parts: []
+
+    defmodule Part do
+      defstruct colour: {0, 0, 0},
+                text: ""
+
+      def wrap(text, opts) do
+        struct!(%Part{text: text}, opts)
+      end
+    end
+
+    def wrap(text, opts) do
+      %Line{parts: [Part.wrap(text, opts)]}
+    end
+  end
+
+  defmodule Output do
+    defstruct operations: []
+
+    alias Mudbrick.ContentStream.{BT, ET}
+    alias Mudbrick.ContentStream.Td
+    alias Mudbrick.ContentStream.Tf
+    alias Mudbrick.ContentStream.{Tj, Apostrophe}
+    alias Mudbrick.ContentStream.TL
+    alias Mudbrick.Font
+
+    def from(
+          %Mudbrick.TextBlock{
+            align: :left,
+            font: font,
+            font_size: font_size,
+            position: {x, y}
+          } = tb
+        ) do
+      output =
+        %Output{}
+        |> start_block()
+        |> add(%Tf{font: font, size: font_size})
+        |> add(%TL{leading: leading(tb)})
+        |> add(%Td{tx: x, ty: y})
+        |> then(fn output ->
+          [%{parts: [first_part]} | rest] = tb.lines
+
+          output
+          |> Output.add(%Tj{font: font, text: first_part.text})
+          |> then(
+            &for %{parts: [part]} <- rest, reduce: &1 do
+              acc -> Output.add(acc, %Apostrophe{font: font, text: part.text})
+            end
+          )
+        end)
+        |> end_block()
+
+      output.operations
+    end
+
+    def from(
+          %Mudbrick.TextBlock{
+            align: :right,
+            font: font,
+            font_size: font_size
+          } = tb
+        ) do
+      output =
+        %Output{}
+        |> start_block()
+        |> add(%Tf{font: font, size: font_size})
+        |> add(%TL{leading: leading(tb)})
+        |> then(fn output ->
+          [%{parts: [first_part]} | rest] = tb.lines
+
+          output
+          |> right_offset(tb, first_part.text, 1)
+          |> Output.add(%Tj{font: font, text: first_part.text})
+          |> end_block()
+          |> then(fn output ->
+            {_line, output} =
+              for %{parts: [part]} <- rest, reduce: {2, output} do
+                {line, acc} ->
+                  {
+                    line + 1,
+                    acc
+                    |> start_block()
+                    |> right_offset(tb, part.text, line)
+                    |> Output.add(%Tj{font: font, text: part.text})
+                    |> end_block()
+                  }
+              end
+
+            output
+          end)
+        end)
+
+      output.operations
+    end
+
+    def add(%__MODULE__{} = output, op) do
+      Map.update!(output, :operations, &[op | &1])
+    end
+
+    def start_block(output) do
+      add(output, %BT{})
+    end
+
+    def end_block(output) do
+      add(output, %ET{})
+    end
+
+    def right_offset(output, tb, text, line) do
+      n = line - 1
+      {x, y} = tb.position
+
+      Output.add(output, %Td{
+        tx: x - Font.width(tb.font, tb.font_size, text),
+        ty: y - leading(tb) * n
+      })
+    end
+
+    defp leading(tb) do
+      tb.font_size * 1.2
+    end
+  end
+
+  alias Line.Part
   alias Mudbrick.ContentStream.{BT, ET}
   alias Mudbrick.ContentStream.Td
   alias Mudbrick.ContentStream.Tf
@@ -16,96 +141,22 @@ defmodule Mudbrick.TextBlock do
     struct!(__MODULE__, opts)
   end
 
-  def write(%__MODULE__{align: :right} = tb, text) do
-    tb = start(tb)
+  def write(tb, text, opts \\ []) do
+    Map.update!(tb, :lines, fn
+      [] ->
+        text
+        |> String.split("\n")
+        |> Enum.map(&Line.wrap(&1, opts))
 
-    split(text, fn
-      [first | rest] ->
-        tb
-        |> right_text(first, 1)
-        |> then(fn context ->
-          {_n, context} =
-            for text <- rest, reduce: {2, context} do
-              {line, acc} ->
-                {
-                  line + 1,
-                  acc
-                  |> start_block()
-                  |> right_text(text, line)
-                }
-            end
+      [%Line{} = previous_line | existing_lines] ->
+        [first_new_line_text | new_line_texts] = String.split(text, "\n")
 
-          context
-        end)
+        previous_line =
+          Map.update!(previous_line, :parts, &[Part.wrap(first_new_line_text, opts) | &1])
+
+        for text <- new_line_texts, reduce: [previous_line | existing_lines] do
+          acc -> [Line.wrap(text, opts) | acc]
+        end
     end)
-  end
-
-  def write(%__MODULE__{position: {x, y}} = tb, text) do
-    tb = start(tb)
-
-    split(text, fn
-      [first | rest] ->
-        tb
-        |> add(%Td{tx: x, ty: y})
-        |> tj(first)
-        |> then(
-          &for text <- rest, reduce: &1 do
-            acc -> apos(acc, text)
-          end
-        )
-    end)
-    |> end_block()
-  end
-
-  defp split(text, f) do
-    f.(String.split(text, "\n"))
-  end
-
-  defp right_text(tb, text, line) do
-    tb
-    |> right_offset(text, line)
-    |> tj(text)
-    |> end_block()
-  end
-
-  defp right_offset(tb, text, line) do
-    n = line - 1
-    {x, y} = tb.position
-
-    add(tb, %Td{
-      tx: x - Font.width(tb.font, tb.font_size, text),
-      ty: y - leading(tb) * n
-    })
-  end
-
-  defp start(%__MODULE__{font: font, font_size: font_size} = tb) do
-    tb
-    |> start_block()
-    |> add(%Tf{font: font, size: font_size})
-    |> add(%TL{leading: leading(tb)})
-  end
-
-  defp start_block(tb) do
-    add(tb, %BT{})
-  end
-
-  defp end_block(tb) do
-    add(tb, %ET{})
-  end
-
-  defp leading(tb) do
-    tb.font_size * 1.2
-  end
-
-  defp add(%__MODULE__{} = tb, op) do
-    Map.update!(tb, :operations, &[op | &1])
-  end
-
-  defp tj(block, text) do
-    add(block, %Tj{font: block.font, text: text})
-  end
-
-  defp apos(block, text) do
-    add(block, %Apostrophe{font: block.font, text: text})
   end
 end
--- test/mudbrick/text_block_test.exs
+++ test/mudbrick/text_block_test.exs
@@ -4,6 +4,32 @@ defmodule Mudbrick.TextBlockTest do
   import Mudbrick.TestHelper, only: [bodoni: 0]
 
   alias Mudbrick.Page
+  alias Mudbrick.TextBlock
+  alias Mudbrick.TextBlock.Line
+  alias Mudbrick.TextBlock.Line.Part
+  alias Mudbrick.TextBlock.Output
+
+  test "writes get divided into lines and parts" do
+    block =
+      TextBlock.new(
+        font_size: 10,
+        position: {400, 500}
+      )
+      |> TextBlock.write("first ", colour: {1, 0, 0})
+      |> TextBlock.write("""
+      line
+      second line
+      """)
+      |> TextBlock.write("""
+      third line\
+      """)
+
+    assert block.lines == [
+             %Line{parts: [%Part{text: "third line"}, %Part{text: ""}]},
+             %Line{parts: [%Part{text: "second line"}]},
+             %Line{parts: [%Part{text: "line"}, %Part{text: "first ", colour: {1, 0, 0}}]}
+           ]
+  end
 
   test "left-aligned newlines become apostrophes" do
     assert [
@@ -17,12 +43,12 @@ defmodule Mudbrick.TextBlockTest do
              "ET"
            ] =
              output(fn font ->
-               Mudbrick.TextBlock.new(
+               TextBlock.new(
                  font: font,
                  font_size: 10,
                  position: {400, 500}
                )
-               |> Mudbrick.TextBlock.write("""
+               |> TextBlock.write("""
                first line
                second line
                """)
@@ -66,44 +92,12 @@ defmodule Mudbrick.TextBlockTest do
              end)
              |> operations()
   end
-    assert [
-             "BT",
-             "/F1 10 Tf",
-             "12.0 TL",
-             "384.82 500.0 Td",
-             "<00A500A500A5> Tj",
-             "ET",
-             "BT",
-             "379.42 488.0 Td",
-             "<013801380138> Tj",
-             "ET",
-             "BT",
-             "306.48 476.0 Td",
-             "<00880055008800550088005500880055008800550088> Tj",
-             "ET",
-             "BT",
-             "400 464.0 Td",
-             "() Tj",
-             "ET"
-           ] =
-             output(fn font ->
-               Mudbrick.TextBlock.new(
-                 font: font,
-                 font_size: 10,
-                 position: {400, 500},
-                 align: :right
-               )
-               |> Mudbrick.TextBlock.write("""
-               aaa
-               www
-               WOWOWOWOWOW
-               """)
-             end)
-             |> operations()
-  end
 
-  defp operations(block) do
-    Enum.reverse(block.operations) |> Enum.map(&Mudbrick.TestHelper.show/1)
+  defp operations(tb) do
+    tb
+    |> Output.from()
+    |> Enum.reverse()
+    |> Enum.map(&Mudbrick.TestHelper.show/1)
   end
 
   defp output(f) when is_function(f) do
@@ -122,7 +116,7 @@ defmodule Mudbrick.TextBlockTest do
     block = f.(contents_obj.value.current_tf.font)
 
     context
-    |> Mudbrick.ContentStream.put(operations: block.operations)
+    |> Mudbrick.ContentStream.put(operations: Output.from(block))
     |> render()
     |> output()
 
