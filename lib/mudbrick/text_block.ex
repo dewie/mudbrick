e689365886c8306646ba66ab9da0792f8ddbe81a
Andrew Bruce <me@andrewbruce.net>
2024-10-28 20:25:03 +0000

    Refactor

--- lib/mudbrick/text_block.ex
+++ lib/mudbrick/text_block.ex
@@ -37,30 +37,36 @@ defmodule Mudbrick.TextBlock do
   end
 
   def write(tb, text, opts \\ []) do
+    line_texts = String.split(text, "\n")
+
     Map.update!(tb, :lines, fn
       [] ->
-        for text <- String.split(text, "\n"), reduce: [] do
-          acc ->
-            [Line.wrap(text, opts) | acc]
-        end
+        add_texts([], line_texts, opts)
 
-      [%Line{} = previous_line | existing_lines] ->
-        [first_new_line_text | new_line_texts] = String.split(text, "\n")
+      existing_lines ->
+        case line_texts do
+          ["" | new_line_texts] ->
+            existing_lines
+            |> add_texts(new_line_texts, opts)
 
-        new_previous_line =
-          if first_new_line_text == "" do
-            previous_line
-          else
-            Map.update!(previous_line, :parts, fn
-              parts ->
-                [Part.wrap(first_new_line_text, opts) | parts]
-            end)
-          end
-
-        for text <- new_line_texts, reduce: [new_previous_line | existing_lines] do
-          acc ->
-            [Line.wrap(text, opts) | acc]
+          [first_new_line_text | new_line_texts] ->
+            existing_lines
+            |> update_previous_line(first_new_line_text, opts)
+            |> add_texts(new_line_texts, opts)
         end
     end)
+  end
+
+  defp update_previous_line([previous_line | existing_lines], first_new_line_text, opts) do
+    [
+      Map.update!(previous_line, :parts, &[Part.wrap(first_new_line_text, opts) | &1])
+      | existing_lines
+    ]
+  end
+
+  defp add_texts(existing_lines, new_line_texts, opts) do
+    for text <- new_line_texts, reduce: existing_lines do
+      acc -> [Line.wrap(text, opts) | acc]
+    end
   end
 end
