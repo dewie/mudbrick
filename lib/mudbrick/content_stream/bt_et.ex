89ade5025b3fe5851fb9ac9297762ee1852a2f09
Andrew Bruce <me@andrewbruce.net>
2024-11-12 22:09:02 +0000

    Remove unused functions

--- lib/mudbrick/content_stream/bt_et.ex
+++ lib/mudbrick/content_stream/bt_et.ex
@@ -2,14 +2,6 @@ defmodule Mudbrick.ContentStream.BT do
   @moduledoc false
   defstruct []
 
-  def open({_doc, content_stream} = context) do
-    context
-    |> Mudbrick.ContentStream.add(%__MODULE__{})
-    |> Mudbrick.ContentStream.add(content_stream.value.current_base_td)
-    |> Mudbrick.ContentStream.add(content_stream.value.current_tf)
-    |> Mudbrick.ContentStream.add(content_stream.value.current_tl)
-  end
-
   defimpl Mudbrick.Object do
     def from(_), do: ["BT"]
   end
@@ -18,12 +10,6 @@ end
 defmodule Mudbrick.ContentStream.ET do
   @moduledoc false
   defstruct []
-
-  def remove(context) do
-    Mudbrick.ContentStream.update_operations(context, fn [%__MODULE__{} | operations] ->
-      operations
-    end)
-  end
 
   defimpl Mudbrick.Object do
     def from(_), do: ["ET"]
