9d3562015b6eda85f97c05e95f797d8f33b198b9
Andrew Bruce <me@andrewbruce.net>
2024-11-13 20:05:15 +0000

    Simplify serialisation of Tj and '

--- lib/mudbrick/content_stream/tj_apostrophe.ex
+++ lib/mudbrick/content_stream/tj_apostrophe.ex
@@ -13,16 +13,16 @@ defmodule Mudbrick.ContentStream.Apostrophe do
 end
 
 defimpl Mudbrick.Object, for: [Mudbrick.ContentStream.Tj, Mudbrick.ContentStream.Apostrophe] do
+  def from(%{text: ""} = op) do
+    ["() ", op.operator]
+  end
+
   def from(op) do
-    if op.font.descendant && String.length(op.text) > 0 do
-      {glyph_ids_decimal, _positions} =
-        OpenType.layout_text(op.font.parsed, op.text)
+    {glyph_ids_decimal, _positions} =
+      OpenType.layout_text(op.font.parsed, op.text)
 
-      glyph_ids_hex = Enum.map(glyph_ids_decimal, &Mudbrick.to_hex/1)
+    glyph_ids_hex = Enum.map(glyph_ids_decimal, &Mudbrick.to_hex/1)
 
-      ["<", glyph_ids_hex, "> ", op.operator]
-    else
-      [Mudbrick.Object.from(op.text), " ", op.operator]
-    end
+    ["<", glyph_ids_hex, "> ", op.operator]
   end
 end
