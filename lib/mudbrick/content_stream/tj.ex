96e9b883bf126bf81250314ddb2c300a523e7978
Andrew Bruce <me@andrewbruce.net>
2024-11-17 12:11:13 +0000

    Fix for Mac, which has type insensitive module lookup?
    ...by removing unused Tj and Apostrophe modules. Tj/TJ apparently can't
    be distinguished on a Mac.

---
+++ lib/mudbrick/content_stream/tj.ex
@@ -0,0 +1,27 @@
+defmodule Mudbrick.ContentStream.TJ do
+  @moduledoc false
+  defstruct font: nil,
+            operator: "TJ",
+            text: nil
+
+  defimpl Mudbrick.Object do
+    def from(%Mudbrick.ContentStream.TJ{text: ""}) do
+      []
+    end
+
+    def from(op) do
+      [
+        "[ ",
+        Mudbrick.Font.kerned(op.font, op.text)
+        |> Enum.map(fn
+          {glyph_id, kerning} ->
+            ["<", glyph_id, "> ", to_string(kerning), " "]
+
+          glyph_id ->
+            ["<", glyph_id, "> "]
+        end),
+        "] TJ"
+      ]
+    end
+  end
+end
