f44ceb2086539ec397c72367d3add60c79c15bdc
Andrew Bruce <me@andrewbruce.net>
2024-12-15 15:13:50 +0000

    Refactor

--- lib/mudbrick/parser.ex
+++ lib/mudbrick/parser.ex
@@ -1,267 +1,100 @@
-defmodule Mudbrick.Parser.Helpers do
-  import NimbleParsec
-
-  def eol, do: string("\n")
-  def whitespace, do: ascii_string([?\n, ?\s], min: 1)
-
-  def version do
-    ignore(string("%PDF-"))
-    |> integer(1)
-    |> ignore(string("."))
-    |> integer(1)
-    |> ignore(eol())
-    |> tag(:version)
-  end
-
-  def name do
-    ignore(string("/"))
-    |> utf8_string(
-      [
-        not: ?\s,
-        not: ?\n,
-        not: ?],
-        not: ?[,
-        not: ?/,
-        not: ?<,
-        not: ?>
-      ],
-      min: 1
-    )
-    |> unwrap_and_tag(:name)
-  end
-
-  def non_negative_integer do
-    ascii_string([?0..?9], min: 1)
-  end
-
-  def negative_integer do
-    string("-")
-    |> concat(non_negative_integer())
-  end
-
-  def indirect_reference do
-    non_negative_integer()
-    |> ignore(whitespace())
-    |> concat(non_negative_integer())
-    |> ignore(whitespace())
-    |> string("R")
-    |> tag(:indirect_reference)
-  end
-
-  def integer do
-    choice([
-      non_negative_integer(),
-      negative_integer()
-    ])
-    |> tag(:integer)
-  end
-
-  def real do
-    choice([
-      non_negative_integer(),
-      negative_integer()
-    ])
-    |> string(".")
-    |> optional(non_negative_integer())
-    |> tag(:real)
-  end
-
-  def number do
-    choice([real(), integer()])
-  end
-
-  def string do
-    ignore(string("("))
-    |> optional(ascii_string([not: ?(, not: ?)], min: 1))
-    |> ignore(string(")"))
-    |> tag(:string)
-  end
-
-  def boolean do
-    choice([
-      string("true") |> replace(true),
-      string("false") |> replace(false)
-    ])
-    |> unwrap_and_tag(:boolean)
-  end
-
-  def xref do
-    ignore(string("xref"))
-    |> ignore(eol())
-  end
-
-  def tf do
-    ignore(string("/F"))
-    |> concat(non_negative_integer())
-    |> ignore(whitespace())
-    |> concat(non_negative_integer())
-    |> ignore(whitespace())
-    |> ignore(string("Tf"))
-    |> tag(:Tf)
-  end
-
-  def tl do
-    real()
-    |> ignore(string(" TL"))
-    |> tag(:TL)
-  end
-
-  def rg_stroking do
-    three_number_operation("RG")
-    |> tag(:RG)
-  end
-
-  def rg_non_stroking do
-    three_number_operation("rg")
-    |> tag(:rg)
-  end
-
-  def m do
-    two_number_operation("m")
-    |> tag(:m)
-  end
-
-  def w do
-    one_number_operation("w")
-    |> tag(:w)
-  end
-
-  def glyph_id_hex do
-    ignore(string("<"))
-    |> ascii_string([?A..?Z, ?0..?9], min: 1)
-    |> ignore(string(">"))
-    |> unwrap_and_tag(:glyph_id)
-  end
-
-  def tj do
-    ignore(string("["))
-    |> ignore(whitespace())
-    |> repeat(
-      choice([
-        glyph_id_hex(),
-        non_negative_integer() |> unwrap_and_tag(:offset)
-      ])
-      |> ignore(whitespace())
-    )
-    |> ignore(string("]"))
-    |> ignore(whitespace())
-    |> ignore(string("TJ"))
-    |> tag(:TJ)
-  end
-
-  def td do
-    two_number_operation("Td")
-    |> tag(:Td)
-  end
-
-  def l do
-    two_number_operation("l")
-    |> tag(:l)
-  end
-
-  def q_push do
-    ignore(string("q")) |> tag(:q)
-  end
-
-  def s do
-    ignore(string("S")) |> tag(:S)
-  end
-
-  def t_star do
-    ignore(string("T*")) |> tag(:TStar)
-  end
-
-  def q_pop do
-    ignore(string("Q")) |> tag(:Q)
-  end
-
-  def graphics_block do
-    q_push()
-    |> ignore(whitespace())
-    |> repeat(
-      choice([
-        l(),
-        m(),
-        rg_non_stroking(),
-        rg_stroking(),
-        s(),
-        w()
-      ])
-      |> ignore(whitespace())
-    )
-    |> concat(q_pop())
-    |> tag(:graphics_block)
-  end
-
-  def text_block do
-    tag(ignore(string("BT")), :BT)
-    |> ignore(whitespace())
-    |> repeat(
-      choice([
-        l(),
-        m(),
-        q_pop(),
-        q_push(),
-        rg_non_stroking(),
-        rg_stroking(),
-        s(),
-        td(),
-        tf(),
-        tj(),
-        tl(),
-        t_star(),
-        w()
-      ])
-      |> ignore(whitespace())
-    )
-    |> tag(ignore(string("ET")), :ET)
-    |> tag(:text_block)
-  end
-
-  defp one_number_operation(operator) do
-    number()
-    |> ignore(whitespace())
-    |> ignore(string(operator))
-  end
-
-  defp two_number_operation(operator) do
-    number()
-    |> ignore(whitespace())
-    |> concat(number())
-    |> ignore(whitespace())
-    |> ignore(string(operator))
-  end
-
-  defp three_number_operation(operator) do
-    number()
-    |> ignore(whitespace())
-    |> concat(number())
-    |> ignore(whitespace())
-    |> concat(number())
-    |> ignore(whitespace())
-    |> ignore(string(operator))
-  end
-end
-
 defmodule Mudbrick.Parser do
   import NimbleParsec
   import Mudbrick.Parser.Helpers
 
   alias Mudbrick.ContentStream.{
-    BT,
-    ET,
     L,
     M,
     QPop,
     QPush,
     Rg,
-    S,
     Td,
     Tf,
     TJ,
     TL,
     W
   }
+
+  def parse(iodata) do
+    {:ok, parsed_items, _rest, %{}, _, _} =
+      iodata
+      |> IO.iodata_to_binary()
+      |> pdf()
+
+    items = Enum.flat_map(parsed_items, &ast_to_mudbrick/1)
+
+    trailer =
+      Enum.find(items, &match?({:Root, _}, &1))
+
+    {:Root, [catalog_ref]} = trailer
+
+    font_files =
+      Enum.filter(
+        items,
+        &match?(%{value: %{additional_entries: %{Subtype: :OpenType}}}, &1)
+      )
+
+    catalog = one(items, catalog_ref)
+
+    [page_tree_ref] = catalog.value[:Pages]
+
+    page_tree = one(items, page_tree_ref)
+
+    page_refs = List.flatten(page_tree.value[:Kids])
+
+    {_, fonts} =
+      for font_file <- font_files, reduce: {1, %{}} do
+        {n, fonts} ->
+          if font_file.value.compress do
+            {n + 1,
+             Map.put(
+               fonts,
+               :"F#{n}",
+               Mudbrick.decompress(font_file.value.data) |> IO.iodata_to_binary()
+             )}
+          else
+            {n + 1, Map.put(fonts, :"F#{n}", font_file.value.data)}
+          end
+      end
+
+    compress? = Enum.any?(font_files, fn f -> f.value.compress end)
+
+    opts = []
+    opts = if map_size(fonts) > 0, do: Keyword.put(opts, :fonts, fonts), else: opts
+    opts = if compress?, do: Keyword.put(opts, :compress, true), else: opts
+
+    for page <- all(items, page_refs), reduce: Mudbrick.new(opts) do
+      acc ->
+        [contents_ref] = page.value[:Contents]
+
+        contents = one(items, contents_ref)
+
+        stream = contents.value
+
+        data =
+          if stream.compress do
+            Mudbrick.decompress(stream.data)
+          else
+            stream.data
+          end
+
+        %Mudbrick.ContentStream{operations: operations} = to_mudbrick(data, :content_blocks)
+
+        Mudbrick.page(acc)
+        |> Mudbrick.ContentStream.update_operations(fn ops ->
+          operations ++ ops
+        end)
+    end
+    |> Mudbrick.Document.finish()
+  end
+
+  def parse(iodata, f) do
+    case iodata
+         |> IO.iodata_to_binary()
+         |> then(&apply(__MODULE__, f, [&1])) do
+      {:ok, resp, _, %{}, _, _} -> resp
+    end
+  end
 
   defparsec(:boolean, boolean())
   defparsec(:number, number())
@@ -351,6 +184,18 @@ defmodule Mudbrick.Parser do
     |> tag(:indirect_object)
   )
 
+  defparsec(
+    :pdf,
+    ignore(version())
+    |> ignore(ascii_string([not: ?\n], min: 1))
+    |> ignore(eol())
+    |> repeat(parsec(:indirect_object) |> ignore(whitespace()))
+    |> ignore(string("xref"))
+    |> ignore(eol())
+    |> eventually(ignore(string("trailer") |> concat(eol())))
+    |> parsec(:dictionary)
+  )
+
   def stream_contents(
         rest,
         [
@@ -369,32 +214,6 @@ defmodule Mudbrick.Parser do
       [binary_slice(rest, 0, bytes_to_read) | results],
       context
     }
-  end
-
-  defparsec(
-    :pdf,
-    ignore(version())
-    |> ignore(ascii_string([not: ?\n], min: 1))
-    |> ignore(eol())
-    |> repeat(parsec(:indirect_object) |> ignore(whitespace()))
-    |> ignore(string("xref"))
-    |> ignore(eol())
-    |> eventually(ignore(string("trailer") |> concat(eol())))
-    |> parsec(:dictionary)
-  )
-
-  defp one(items, ref) do
-    Enum.find(items, &match?(%{ref: ^ref}, &1))
-  end
-
-  defp all(items, refs) do
-    Enum.filter(items, fn
-      %{ref: ref} ->
-        ref in refs
-
-      _ ->
-        false
-    end)
   end
 
   def extract_text(iodata) do
@@ -436,94 +255,25 @@ defmodule Mudbrick.Parser do
     Enum.reverse(text_items)
   end
 
-  def parse(iodata) do
-    {:ok, parsed_items, _rest, %{}, _, _} =
-      iodata
-      |> IO.iodata_to_binary()
-      |> pdf()
-
-    items = Enum.flat_map(parsed_items, &ast_to_mudbrick/1)
-
-    trailer =
-      Enum.find(items, &match?({:Root, _}, &1))
-
-    {:Root, [catalog_ref]} = trailer
-
-    font_files =
-      Enum.filter(
-        items,
-        &match?(%{value: %{additional_entries: %{Subtype: :OpenType}}}, &1)
-      )
-
-    catalog = one(items, catalog_ref)
-
-    [page_tree_ref] = catalog.value[:Pages]
-
-    page_tree = one(items, page_tree_ref)
-
-    page_refs = List.flatten(page_tree.value[:Kids])
-
-    {_, fonts} =
-      for font_file <- font_files, reduce: {1, %{}} do
-        {n, fonts} ->
-          if font_file.value.compress do
-            {n + 1,
-             Map.put(
-               fonts,
-               :"F#{n}",
-               Mudbrick.decompress(font_file.value.data) |> IO.iodata_to_binary()
-             )}
-          else
-            {n + 1, Map.put(fonts, :"F#{n}", font_file.value.data)}
-          end
-      end
-
-    compress? = Enum.any?(font_files, fn f -> f.value.compress end)
-
-    opts = []
-    opts = if map_size(fonts) > 0, do: Keyword.put(opts, :fonts, fonts), else: opts
-    opts = if compress?, do: Keyword.put(opts, :compress, true), else: opts
-
-    for page <- all(items, page_refs), reduce: Mudbrick.new(opts) do
-      acc ->
-        [contents_ref] = page.value[:Contents]
-
-        contents = one(items, contents_ref)
-
-        stream = contents.value
-
-        data =
-          if stream.compress do
-            Mudbrick.decompress(stream.data)
-          else
-            stream.data
-          end
-
-        %Mudbrick.ContentStream{operations: operations} = to_mudbrick(data, :content_blocks)
-
-        Mudbrick.page(acc)
-        |> Mudbrick.ContentStream.update_operations(fn ops ->
-          operations ++ ops
-        end)
-    end
-    |> Mudbrick.Document.finish()
-  end
-
-  def parse(iodata, f) do
-    case iodata
-         |> IO.iodata_to_binary()
-         |> then(&apply(__MODULE__, f, [&1])) do
-      {:ok, resp, _, %{}, _, _} -> resp
-    end
-  end
-
   def to_mudbrick(iodata, f),
     do:
       iodata
       |> parse(f)
       |> ast_to_mudbrick()
 
-  defp ast_to_operator({:BT, []}), do: %BT{}
+  defp one(items, ref) do
+    Enum.find(items, &match?(%{ref: ^ref}, &1))
+  end
+
+  defp all(items, refs) do
+    Enum.filter(items, fn
+      %{ref: ref} ->
+        ref in refs
+
+      _ ->
+        false
+    end)
+  end
 
   defp ast_to_operator({:m, [x, y]}),
     do: %M{
@@ -543,16 +293,9 @@ defmodule Mudbrick.Parser do
       b: ast_to_mudbrick(b)
     }
 
-  defp ast_to_operator({:S, []}), do: %S{}
   defp ast_to_operator({:Td, [x, y]}), do: %Td{tx: ast_to_mudbrick(x), ty: ast_to_mudbrick(y)}
-
   defp ast_to_operator({:w, number}), do: %W{width: ast_to_mudbrick(number)}
-
-  defp ast_to_operator({:q, []}), do: %QPush{}
-  defp ast_to_operator({:Q, []}), do: %QPop{}
-
   defp ast_to_operator({:Tf, [index, size]}), do: %Tf{font_identifier: :"F#{index}", size: size}
-
   defp ast_to_operator({:TL, leading}), do: %TL{leading: ast_to_mudbrick(leading)}
 
   defp ast_to_operator({:rg, components}),
@@ -576,7 +319,11 @@ defmodule Mudbrick.Parser do
     }
   end
 
-  defp ast_to_operator({:ET, []}), do: %ET{}
+  defp ast_to_operator({:Q, []}), do: %QPop{}
+  defp ast_to_operator({:q, []}), do: %QPush{}
+
+  defp ast_to_operator({op, []}),
+    do: struct!(Module.safe_concat([Mudbrick.ContentStream, op]), [])
 
   defp ast_to_mudbrick([{block_type, _operations} | _rest] = input)
        when block_type in [:text_block, :graphics_block] do
---
+++ lib/mudbrick/parser/helpers.ex
@@ -0,0 +1,244 @@
+defmodule Mudbrick.Parser.Helpers do
+  import NimbleParsec
+
+  def eol, do: string("\n")
+  def whitespace, do: ascii_string([?\n, ?\s], min: 1)
+
+  def version do
+    ignore(string("%PDF-"))
+    |> integer(1)
+    |> ignore(string("."))
+    |> integer(1)
+    |> ignore(eol())
+    |> tag(:version)
+  end
+
+  def name do
+    ignore(string("/"))
+    |> utf8_string(
+      [
+        not: ?\s,
+        not: ?\n,
+        not: ?],
+        not: ?[,
+        not: ?/,
+        not: ?<,
+        not: ?>
+      ],
+      min: 1
+    )
+    |> unwrap_and_tag(:name)
+  end
+
+  def non_negative_integer do
+    ascii_string([?0..?9], min: 1)
+  end
+
+  def negative_integer do
+    string("-")
+    |> concat(non_negative_integer())
+  end
+
+  def indirect_reference do
+    non_negative_integer()
+    |> ignore(whitespace())
+    |> concat(non_negative_integer())
+    |> ignore(whitespace())
+    |> string("R")
+    |> tag(:indirect_reference)
+  end
+
+  def integer do
+    choice([
+      non_negative_integer(),
+      negative_integer()
+    ])
+    |> tag(:integer)
+  end
+
+  def real do
+    choice([
+      non_negative_integer(),
+      negative_integer()
+    ])
+    |> string(".")
+    |> optional(non_negative_integer())
+    |> tag(:real)
+  end
+
+  def number do
+    choice([real(), integer()])
+  end
+
+  def string do
+    ignore(string("("))
+    |> optional(ascii_string([not: ?(, not: ?)], min: 1))
+    |> ignore(string(")"))
+    |> tag(:string)
+  end
+
+  def boolean do
+    choice([
+      string("true") |> replace(true),
+      string("false") |> replace(false)
+    ])
+    |> unwrap_and_tag(:boolean)
+  end
+
+  def xref do
+    ignore(string("xref"))
+    |> ignore(eol())
+  end
+
+  def tf do
+    ignore(string("/F"))
+    |> concat(non_negative_integer())
+    |> ignore(whitespace())
+    |> concat(non_negative_integer())
+    |> ignore(whitespace())
+    |> ignore(string("Tf"))
+    |> tag(:Tf)
+  end
+
+  def tl do
+    real()
+    |> ignore(string(" TL"))
+    |> tag(:TL)
+  end
+
+  def rg_stroking do
+    three_number_operation("RG")
+    |> tag(:RG)
+  end
+
+  def rg_non_stroking do
+    three_number_operation("rg")
+    |> tag(:rg)
+  end
+
+  def m do
+    two_number_operation("m")
+    |> tag(:m)
+  end
+
+  def w do
+    one_number_operation("w")
+    |> tag(:w)
+  end
+
+  def glyph_id_hex do
+    ignore(string("<"))
+    |> ascii_string([?A..?Z, ?0..?9], min: 1)
+    |> ignore(string(">"))
+    |> unwrap_and_tag(:glyph_id)
+  end
+
+  def tj do
+    ignore(string("["))
+    |> ignore(whitespace())
+    |> repeat(
+      choice([
+        glyph_id_hex(),
+        non_negative_integer() |> unwrap_and_tag(:offset)
+      ])
+      |> ignore(whitespace())
+    )
+    |> ignore(string("]"))
+    |> ignore(whitespace())
+    |> ignore(string("TJ"))
+    |> tag(:TJ)
+  end
+
+  def td do
+    two_number_operation("Td")
+    |> tag(:Td)
+  end
+
+  def l do
+    two_number_operation("l")
+    |> tag(:l)
+  end
+
+  def q_push do
+    ignore(string("q")) |> tag(:q)
+  end
+
+  def s do
+    ignore(string("S")) |> tag(:S)
+  end
+
+  def t_star do
+    ignore(string("T*")) |> tag(:TStar)
+  end
+
+  def q_pop do
+    ignore(string("Q")) |> tag(:Q)
+  end
+
+  def graphics_block do
+    q_push()
+    |> ignore(whitespace())
+    |> repeat(
+      choice([
+        l(),
+        m(),
+        rg_non_stroking(),
+        rg_stroking(),
+        s(),
+        w()
+      ])
+      |> ignore(whitespace())
+    )
+    |> concat(q_pop())
+    |> tag(:graphics_block)
+  end
+
+  def text_block do
+    tag(ignore(string("BT")), :BT)
+    |> ignore(whitespace())
+    |> repeat(
+      choice([
+        l(),
+        m(),
+        q_pop(),
+        q_push(),
+        rg_non_stroking(),
+        rg_stroking(),
+        s(),
+        td(),
+        tf(),
+        tj(),
+        tl(),
+        t_star(),
+        w()
+      ])
+      |> ignore(whitespace())
+    )
+    |> tag(ignore(string("ET")), :ET)
+    |> tag(:text_block)
+  end
+
+  defp one_number_operation(operator) do
+    number()
+    |> ignore(whitespace())
+    |> ignore(string(operator))
+  end
+
+  defp two_number_operation(operator) do
+    number()
+    |> ignore(whitespace())
+    |> concat(number())
+    |> ignore(whitespace())
+    |> ignore(string(operator))
+  end
+
+  defp three_number_operation(operator) do
+    number()
+    |> ignore(whitespace())
+    |> concat(number())
+    |> ignore(whitespace())
+    |> concat(number())
+    |> ignore(whitespace())
+    |> ignore(string(operator))
+  end
+end
